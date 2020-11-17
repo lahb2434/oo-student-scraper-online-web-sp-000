c8dfc950be360fefc3f472ea3c93075b5f2da5f9
Chris Houghton <chrishoughton2434@gmail.com>
2020-11-17 18:45:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,7 +7,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    binding.pry
   end
 
   def self.create_from_collection(students_array)

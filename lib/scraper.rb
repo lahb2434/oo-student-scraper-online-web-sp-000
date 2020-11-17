6b00082d77ab56d3a4024834687aa43393ea52f7
Chris Houghton <chrishoughton2434@gmail.com>
2020-11-17 16:47:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,16 @@
 require 'open-uri'
 require 'pry'
 
+# student body: student.css("div.student-card").first
+
+
 class Scraper
 
   def self.scrape_index_page(index_url)
+    student_index_array = []
+    Nokogiri::HTML(open(index_url)).css("div.student-card").each do |person|
     
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
